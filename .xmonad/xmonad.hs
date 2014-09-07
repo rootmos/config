83b624023cfd5e525e06e58bb9e1c02fb7eb6251
Gustav Behm <gustav.behm@ericsson.com>
2014-09-07 13:09:24 +0200

    Added dzen and two shifts to the xmonad conf.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,11 @@
+-- Import statements
+import Dzen
 import XMonad
 import Control.Monad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.UrgencyHook
+import XMonad.Util.Run
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import XMonad.Hooks.ICCCMFocus
@@ -25,11 +31,15 @@ makeWorkspaceKeys mask =
 
 myManageHook = composeAll . concat $
     [
-        [className =? b --> viewShift "web" | b <- myClassWebShifts  ]
+        [className =? b --> viewShift "web" | b <- myClassWebShifts],
+        [className =? b --> viewShift "mail" | b <- myClassMailShifts],
+        [className =? b --> viewShift "chat" | b <- myClassChatShifts]
     ]
     where
         viewShift = doF . liftM2 (.) W.greedyView W.shift
-        myClassWebShifts = ["Chromium-browser"]
+        myClassWebShifts = ["Chrome"]
+        myClassMailShifts = ["Thunderbird"]
+        myClassChatShifts = ["Pidgin"]
 
 keysToAdd x =
     makeWorkspaceKeys mod4Mask myWorkspaces
@@ -42,22 +52,56 @@ keysToRemove x =
 strippedKeys x = foldr M.delete (keys defaultConfig x) (keysToRemove x)
 myKeys x = M.union (strippedKeys x) (M.fromList (keysToAdd x))
 
+ 
+-- Workspace dzen bar
+myStatusBar = DzenConf {
+      x_position = Just 0
+    , y_position = Just 0
+    , width      = Just 1920
+    , height     = Just 13
+    , alignment  = Just LeftAlign
+    , font       = Just "Bitstream Sans Vera:pixelsize=10"
+    , fg_color   = Just "#ffffff"
+    , bg_color   = Just "#000000"
+    , exec       = []
+    , addargs    = []
+}
+ 
+-- Pretty printer for dzen workspace bar
+myPrettyPrinter h = dzenPP {
+      ppOutput          = hPutStrLn h
+    , ppCurrent         = dzenColor "#000000" "#e5e5e5"
+    , ppHidden          = dzenColor "#e5e5e5" "#000000"
+    , ppHiddenNoWindows = dzenColor "#444444" "#000000"
+    , ppUrgent          = dzenColor "#ff0000" "#000000". dzenStrip
+    , ppWsSep           = " "
+    , ppSep             = " | "
+}
+
 myLogHook = logHook defaultConfig <+> ewmhDesktopsLogHook
+myDzenLogHook h = dynamicLogWithPP $ myPrettyPrinter h
 
 myStartupHook config = do
    startupHook config   
    spawn "xreaddb"
    spawn "start-xsettingsd"
+
+myLayoutHook config = avoidStruts $ layoutHook config
+
  
 main = do
+    workspaceBar <- spawnDzen myStatusBar
     xmonad $ defaultConfig
         {
             terminal = myTerminal,
             workspaces = map fst myWorkspaces,
-            manageHook = myManageHook,
+            manageHook = myManageHook <+> manageDocks,
             keys = myKeys,
             focusFollowsMouse = False,
-            logHook = myLogHook,
-            startupHook = myStartupHook defaultConfig 
-
+            logHook = myLogHook <+> myDzenLogHook workspaceBar,
+            startupHook = myStartupHook defaultConfig,
+            layoutHook = myLayoutHook defaultConfig
         }
+ 
+
+
