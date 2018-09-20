37c742981fab8b8bb43cd67d055298a7aace8774
Gustav Behm <gustav.behm@gmail.com>
2018-09-20 21:51:22 +0200

    Add scripts to select appropriate passphrase

--- bin/lib.sh
+++ bin/lib.sh
@@ -13,3 +13,15 @@ is_unlocked() {
 is_mounted() {
     findmnt --noheadings $1 &> /dev/null
 }
+
+get_passphrase() {
+    pinentry-gtk-2 <<EOF | grep -v "^OK" | grep "^D " | tail -c+3
+SETPROMPT ${1-PIN}
+SETOK ${2-OK}
+GETPIN
+EOF
+}
+
+is_yubikey_present() {
+    lsusb | grep -q Yubikey
+}
