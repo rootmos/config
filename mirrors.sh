5a28e2c63926b964d9f4ce7ccb43399f3997d24b
Gustav Behm <me@rootmos.io>
2019-11-09 15:14:01 +0100

    Use numbered backups when refreshing the mirrorlist

--- mirrors.sh
+++ mirrors.sh
@@ -3,4 +3,4 @@
 curl -s 'https://www.archlinux.org/mirrorlist/?country=DE&country=SE&protocol=https' \
     | sed 's/^#Server/Server/' \
     | rankmirrors -n 10 - \
-    | sudo install --backup /dev/stdin /etc/pacman.d/mirrorlist
+    | sudo install --backup=numbered /dev/stdin /etc/pacman.d/mirrorlist
