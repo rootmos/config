342c387b35b697fc489747caf564a7b4d77f972b
Gustav Behm <me@rootmos.io>
2020-01-15 10:05:23 +0100

    Clean up keybindings in the xmonad config: set everything explicitly

--- .xmonad/src/Main.hs
+++ .xmonad/src/Main.hs
@@ -4,19 +4,20 @@ module Main where
 
 import Control.Monad ( liftM2, (<=<) )
 import Data.Monoid ( Endo )
+import Data.Functor ( (<&>) )
 import Graphics.X11.ExtraTypes.XF86
 import Network.HostName ( getHostName )
-import System.Directory ( getAppUserDataDirectory, doesFileExist )
+import System.Directory ( getAppUserDataDirectory, doesFileExist, getHomeDirectory )
+import System.FilePath.Posix ( (</>) )
 import Text.Printf ( printf )
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.FadeInactive ( fadeInactiveLogHook )
-import XMonad.Hooks.ManageDocks ( AvoidStruts, avoidStruts, docks, ToggleStruts(..) )
+import XMonad.Hooks.ManageDocks ( avoidStruts, docks, ToggleStruts(..) )
 import XMonad.Hooks.SetWMName ( setWMName )
-import XMonad.Layout.LayoutModifier ( ModifiedLayout )
 import XMonad.Layout.NoBorders ( smartBorders, noBorders )
 import XMonad.Layout.SimpleFloat ( simpleFloat )
-import XMonad.Util.Run ( spawnPipe )
+import XMonad.Util.Run ( spawnPipe, hPutStrLn )
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
@@ -51,54 +52,40 @@ myManageHooks = composeAll $ [ className =? "Chromium" --> doShift "w"
 
 makeWorkspaceKeys :: ButtonMask -> [(String,KeySym)] -> [((ButtonMask,KeySym), X())]
 makeWorkspaceKeys mask ws = gotoKeys ws ++ moveKeys ws
-    where
-        gotoKeys = map (\(name, key) -> ((mask, key), windows $ W.view name))
-        moveKeys = map (\(name, key) -> ((mask .|. shiftMask, key), windows $ W.shift name))
+    where gotoKeys = map (\(name, key) -> ((mask, key), windows $ W.view name))
+          moveKeys = map (\(name, key) -> ((mask .|. shiftMask, key), windows $ W.shift name))
 
-myKeys :: XConfig Layout -> M.Map (ButtonMask, KeySym) (X ())
-myKeys x = M.union (M.fromList keysToAdd) strippedKeys
-  where
-    strippedKeys = foldr M.delete (keys def x) keysToRemove
-    keysToRemove = [ (modMask x, xK_comma)
-                   , (modMask x, xK_period)
-                   ]
-    keysToAdd = makeWorkspaceKeys mod4Mask myWorkspaces
-                ++ [ ((0, xF86XK_MonBrightnessUp), spawn "sudo /home/gustav/bin/xbacklight -inc 10")
-                   , ((0, xF86XK_MonBrightnessDown), spawn "sudo /home/gustav/bin/xbacklight -dec 10")
-                   , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 5%+")
-                   , ((0, xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 5%-")
-                   , ((0, xF86XK_AudioMute), spawn "amixer -D pulse sset Master toggle")
-                   , ((0, xF86XK_Display), spawn "/home/gustav/.local/bin/displayswitcheroo")
-                   , ((0, xK_F11), spawn "/home/gustav/bin/screenshot")
-                   , ((modMask x, xK_d), spawn "docs")
-                   , ((modMask x, xK_w), spawn "/home/gustav/bin/netctl-switch-to-menu")
-                   , ((modMask x, xK_period), spawn "/home/gustav/bin/pass-pick")
-                   , ((0, xF86XK_WLAN), spawn "sudo /home/gustav/bin/wifi-fix")
-                   , ((mod4Mask, xK_comma), sendMessage (IncMasterN 1))
-                   , ((mod4Mask, xK_period), sendMessage (IncMasterN (-1)))
-                   ]
-
-toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
-toggleStrutsKey XConfig { modMask = modm } = (modm, xK_b)
-
--- modified version of https://hackage.haskell.org/package/xmonad-contrib-0.13/docs/src/XMonad-Hooks-DynamicLog.html#statusBar
--- that does not do any ppOutput
-statusBar' :: (LayoutClass l Window, MonadIO m)
-           => String
-           -> PP
-           -> (XConfig Layout -> (ButtonMask, KeySym))
-           -> XConfig l
-           -> m (XConfig (ModifiedLayout AvoidStruts l))
-statusBar' cmd pp k conf = do
-    _ <- spawnPipe cmd
-    return $ docks $ conf
-        { layoutHook = avoidStruts (layoutHook conf)
-        , logHook = do
-            logHook conf
-            dynamicLogWithPP pp { ppOutput = \_ -> return ()}
-        , keys = liftM2 M.union keys' (keys conf)
-        }
-          where keys' = (`M.singleton` sendMessage ToggleStruts) . k
+myKeys :: FilePath -> FilePath
+       -> XConfig l -> M.Map (ButtonMask, KeySym) (X ())
+myKeys bin localBin XConfig { terminal = t } = M.fromList $ makeWorkspaceKeys mod4Mask myWorkspaces ++
+  [ ((mod1Mask .|. shiftMask, xK_Return), spawn t)
+  , ((mod1Mask .|. shiftMask, xK_c), kill)
+  , ((mod1Mask, xK_space), sendMessage NextLayout)
+  , ((mod1Mask, xK_Tab), windows W.focusDown)
+  , ((mod1Mask .|. shiftMask, xK_Tab), windows W.focusUp )
+  , ((mod1Mask, xK_j), windows W.focusDown)
+  , ((mod1Mask, xK_k), windows W.focusUp )
+  , ((mod1Mask, xK_m), windows W.focusMaster )
+  , ((mod1Mask, xK_Return), windows W.swapMaster)
+  , ((mod1Mask .|. shiftMask, xK_j), windows W.swapDown )
+  , ((mod1Mask .|. shiftMask, xK_k), windows W.swapUp )
+  , ((mod1Mask, xK_h), sendMessage Shrink)
+  , ((mod1Mask, xK_l), sendMessage Expand)
+  , ((mod1Mask, xK_t), withFocused $ windows . W.sink)
+  , ((mod4Mask, xK_b), sendMessage ToggleStruts)
+  , ((mod4Mask, xK_comma), sendMessage (IncMasterN 1))
+  , ((mod4Mask, xK_period), sendMessage (IncMasterN (-1)))
+  , ((mod4Mask, xK_d), spawn "docs")
+  , ((mod4Mask, xK_period), spawn $ bin </> "pass-pick")
+  , ((0, xF86XK_MonBrightnessUp), spawn $ bin </> "xbacklight -inc 10")
+  , ((0, xF86XK_MonBrightnessDown), spawn $ bin </> "xbacklight -dec 10")
+  , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 5%+")
+  , ((0, xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 5%-")
+  , ((0, xF86XK_AudioMute), spawn "amixer -D pulse sset Master toggle")
+  , ((0, xF86XK_Display), spawn $ localBin </> "displayswitcheroo")
+  , ((0, xK_F11), spawn $ bin </> "screenshot")
+  , ((0, xF86XK_WLAN), spawn $ bin </> "wifi-fix")
+  ]
 
 currentWidth :: IO Int
 currentWidth = do
@@ -110,45 +97,44 @@ currentWidth = do
       return $ monitorWidth monitor
     _ -> return 1910
 
-bars :: LayoutClass l Window
-     => XConfig l
-     -> IO (XConfig (ModifiedLayout AvoidStruts (ModifiedLayout AvoidStruts l)))
+bars :: XConfig l -> IO (XConfig l)
 bars conf = do
-  h <- getHostName
   width <- currentWidth
-  (statusBar' (myStatusBar h width) pp toggleStrutsKey conf)
-    >>= (statusBar (myXmonadBar width) pp toggleStrutsKey)
+  h <- spawnPipe $ myXmonadBar width
+  _ <- getHostName >>= spawnPipe . myStatusBar width
+  return $ docks $ conf { logHook = logHook conf >> dynamicLogWithPP pp { ppOutput = hPutStrLn h } }
     where right = 900
           left width = width - right
           font = "-*-helvetica-*-r-*-*-14-*-*-*-*-*-*-*" :: String
           dzenCmd = printf "dzen2 -e 'onstart=lower' -dock -fg '#FFFFFF' -bg '#1B1D1E' -fn '%s'" font :: String
           myXmonadBar width = printf "%s -x '0' -y '0' -w '%d' -ta 'l'" dzenCmd (left width)
-          myStatusBar h width = printf "conky -c ~/.xmonad/conky.%s.config 0>/dev/null | %s -x '%d' -y '0' -w '%d' -ta 'r'" h dzenCmd (left width) right
-          pp = def { ppCurrent           =   dzenColor "#ebac54" "#1B1D1E"
-                   , ppVisible           =   dzenColor "white" "#1B1D1E"
-                   , ppHidden            =   dzenColor "white" "#1B1D1E"
-                   , ppHiddenNoWindows   =   dzenColor "#7b7b7b" "#1B1D1E"
-                   , ppUrgent            =   dzenColor "#ff0000" "#1B1D1E"
-                   , ppLayout            =   dzenColor "#7b7b7b" "#1B1D1E"
-                   , ppWsSep             =   " "
-                   , ppSep               =   " | "
-                   , ppTitle             =   dzenColor "white" "#1B1D1E" . dzenEscape
+          myStatusBar width h = printf "conky -c ~/.xmonad/conky.%s.config 0>/dev/null | %s -x '%d' -y '0' -w '%d' -ta 'r'" h dzenCmd (left width) right
+          pp = def { ppCurrent         = dzenColor "#ebac54" "#1B1D1E"
+                   , ppVisible         = dzenColor "white" "#1B1D1E"
+                   , ppHidden          = dzenColor "white" "#1B1D1E"
+                   , ppHiddenNoWindows = dzenColor "#7b7b7b" "#1B1D1E"
+                   , ppUrgent          = dzenColor "#ff0000" "#1B1D1E"
+                   , ppLayout          = dzenColor "#7b7b7b" "#1B1D1E"
+                   , ppWsSep           = " "
+                   , ppSep             = " | "
+                   , ppTitle           = dzenColor "white" "#1B1D1E" . dzenEscape
                    }
 
 main :: IO ()
 main = do
-  fn <- getAppUserDataDirectory "xmonad/border-width"
-  bw <- doesFileExist fn >>= \case
-    False -> return 4
-    True -> read <$> readFile fn
+  (bin, localBin) <- getHomeDirectory <&> \h -> (h </> "bin", h </> ".local" </> "bin")
+  bw <- getAppUserDataDirectory "xmonad" <&> (\x -> x </> "border-width") >>= \fn ->
+    doesFileExist fn >>= \case
+      False -> return 4
+      True -> read <$> readFile fn
   xmonad <=< bars $
     def { terminal = "st"
         , workspaces = fst <$> myWorkspaces
         , manageHook = myManageHooks <+> manageHook def
-        , layoutHook = smartBorders (Tall 1 (2/100) (1/2)) ||| noBorders Full ||| simpleFloat
+        , layoutHook = avoidStruts $ smartBorders (Tall 1 (2/100) (1/2)) ||| noBorders Full ||| simpleFloat
         , logHook = fadeInactiveLogHook 0xdddddddd <+> logHook def
-        , startupHook = (composeAll [ setWMName "LG3D" ]) <+> startupHook def
-        , keys = myKeys
+        , startupHook = composeAll [ setWMName "LG3D" ] <+> startupHook def
+        , keys = myKeys bin localBin
         , focusFollowsMouse = False
         , focusedBorderColor = "red"
         , borderWidth = bw
