921d2c9bdaeadffbcfa78d842143efb5062b2648
Gustav Behm <me@rootmos.io>
2023-07-04 15:59:01 +0200

    Bind F9-F11 to the new action script

--- .xmonad/src/Main.hs
+++ .xmonad/src/Main.hs
@@ -76,8 +76,9 @@ myKeys _ bin localBin XConfig { terminal = t } = M.fromList $ makeWorkspaceKeys 
   , ((0, xF86XK_AudioMute), spawn $ bin </> "volume m")
   , ((0, xF86XK_Display), spawn $ localBin </> "displayswitcheroo")
   , ((0, xF86XK_WebCam), spawn $ bin </> "bt")
-  --, ((0, xK_F9), spawn $ localBin </> "turnc split")
-  , ((0, xK_F11), spawn $ bin </> "screenshot")
+  , ((0, xK_F9), spawn $ localBin </> "action trigger f9")
+  , ((0, xK_F10), spawn $ localBin </> "action trigger f10")
+  , ((0, xK_F11), spawn $ localBin </> "action trigger f11")
   , ((0, xF86XK_WLAN), spawn $ bin </> "wifi-fix")
   ]
 
