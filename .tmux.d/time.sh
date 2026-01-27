3dec5e9307b18a2e5e8d4401e0e5e31ae3158973
Gustav Behm <me@rootmos.io>
2026-01-27 13:20:50 +0100

    Rework the tmx time session script

--- .tmux.d/time.sh
+++ .tmux.d/time.sh
@@ -1,9 +1,12 @@
 RUTIN="$HOME/git/rutin"
 tmux move-window -t 0
 tmux new-window -n "klocka" -c "$RUTIN/klocka"
-tmux split-window -d -h -c "$RUTIN/progress"
-tmux split-window -d -v -c "$RUTIN/be-on-time" -t 2 -b
+tmux split-window -d -v -c "$RUTIN/progress"
+tmux split-window -d -h -c "$RUTIN/be-on-time" -t 1
 
 tmux send-keys -t 1 "C-l" "klocka TDFJQ"
 tmux send-keys -t 2 "C-l" "./be-on-time "
-tmux send-keys -t 3 "C-l" "progress add "
+tmux send-keys -t 3 "C-l" "progress log" Enter "progress add "
+
+tmux select-pane -t 1
+tmux resize-pane -Z -t 1
